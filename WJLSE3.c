91b6982153f0da0878921e4ec5ec9bbd58daef57
Jielin-Code <68259621+Jielin-Code@users.noreply.github.com>
2021-09-28 16:27:07 +0800

    Update WJLSE3.c

--- WJLSE3.c
+++ WJLSE3.c
@@ -2,45 +2,116 @@
 #include "WJLSE.h"
 #include "WJLSD.h"
 
-unsigned char* encrypt(const char *PasswordBuff, unsigned char *InByteArray, unsigned int InByteArray_length, unsigned int *OutByteArray_length)
+unsigned char* encrypt(const char *PasswordBuff, unsigned char *InByteArray, unsigned int InByteArray_length, unsigned int *OutByteArray_length, unsigned int RandomBytes_Length)
 {
 	WJLEnCoder coder;
 	int PasswordBuff_Length = 0;
+	unsigned char *OutBuff;
 	while (PasswordBuff[PasswordBuff_Length] != '\0') PasswordBuff_Length++;
+
+	// Initialize variable
+	coder.symbolsMaxCount = 256;
+	coder.SymbolsFreqArray = (unsigned int *)malloc(sizeof(unsigned int) * (coder.symbolsMaxCount + 1));
+	coder.encodeOutBufferArray = (unsigned char *)malloc((double)InByteArray_length * 1.2);
+	coder.Sbox = (unsigned char *)malloc( coder.symbolsMaxCount );
+	if(RandomBytes_Length < 64){
+		coder.RandomBytes_Length = 64;
+	}else{
+		coder.RandomBytes_Length = RandomBytes_Length;
+	}
+	coder.RandomBytes = (unsigned char *)malloc(coder.RandomBytes_Length);
+	if(coder.SymbolsFreqArray == NULL || coder.encodeOutBufferArray == NULL || coder.Sbox == NULL || coder.RandomBytes == NULL){
+		if(coder.SymbolsFreqArray) free(coder.SymbolsFreqArray);
+		if(coder.encodeOutBufferArray) free(coder.encodeOutBufferArray);
+		if(coder.Sbox) free(coder.Sbox);
+		if(coder.RandomBytes) free(coder.RandomBytes);
+		return 0;
+	}
 	// Initialize WJLEnCoder
-	InitializeWJLEnCoder(&coder, InByteArray_length);
+	InitializeWJLEnCoder(&coder, PasswordBuff, PasswordBuff_Length);
 	// Encrypt to 64 random numbers 64 = 512bit
-	EncryptingTheRandomBytes(&coder,PasswordBuff, PasswordBuff_Length, 64);
-	// Custom hash values of 65 bytes
-	EncryptingTheHashValue(&coder, PasswordBuff, PasswordBuff_Length, 65);
+	EncryptingTheRandomBytes(&coder, PasswordBuff, PasswordBuff_Length);
 	// Encrypt the InByteArray_length
-	EncryptingFileLength(&coder, InByteArray_length);
+	EncryptingFileLength(&coder, InByteArray_length, PasswordBuff, PasswordBuff_Length);
 	// Encrypt the InByteArray
-	return WJLWeightedEncode(&coder, PasswordBuff, PasswordBuff_Length, InByteArray, InByteArray_length, OutByteArray_length);
+	WJLWeightedEncode(&coder, PasswordBuff, PasswordBuff_Length, InByteArray, InByteArray_length, OutByteArray_length);
+	
+	OutBuff = (unsigned char *)malloc( *OutByteArray_length );
+	if(OutBuff == NULL){
+		if(coder.SymbolsFreqArray) free(coder.SymbolsFreqArray);
+		if(coder.encodeOutBufferArray) free(coder.encodeOutBufferArray);
+		if(coder.Sbox) free(coder.Sbox);
+		if(coder.RandomBytes) free(coder.RandomBytes);
+		return 0;
+	}
+	memcpy(OutBuff, coder.encodeOutBufferArray, *OutByteArray_length);
+
+	if(coder.SymbolsFreqArray) free(coder.SymbolsFreqArray);
+	if(coder.encodeOutBufferArray) free(coder.encodeOutBufferArray);
+	if(coder.Sbox) free(coder.Sbox);
+	if(coder.RandomBytes) free(coder.RandomBytes);
+	return OutBuff;
 }
 
-unsigned char* decrypt(const char *PasswordBuff, unsigned char *InByteArray, unsigned int InByteArray_length, unsigned int *OutByteArray_length)
+unsigned char* decrypt(const char *PasswordBuff, unsigned char *InByteArray, unsigned int InByteArray_length, unsigned int *OutByteArray_length, unsigned int RandomBytes_Length)
 {
 	WJLDeCoder coder;
 	int PasswordBuff_Length = 0, InArraylength = 0;
+	unsigned char *OutBuff;
 	while (PasswordBuff[PasswordBuff_Length] != '\0') PasswordBuff_Length++;
+	// Initialize variable
+	coder.symbolsMaxCount = 256;
+	coder.SymbolsFreqArray = (unsigned int *)malloc(sizeof(unsigned int) * (coder.symbolsMaxCount + 1));
+	coder.decodeOutBufferArray = (unsigned char *)malloc( InByteArray_length * 8 );
+	coder.Sbox = (unsigned char *)malloc( coder.symbolsMaxCount );
+	if(RandomBytes_Length < 64){
+		coder.RandomBytes_Length = 64;
+	}else{
+		coder.RandomBytes_Length = RandomBytes_Length;
+	}
+	if(coder.SymbolsFreqArray == NULL || coder.decodeOutBufferArray == NULL || coder.Sbox == NULL){
+		if(coder.SymbolsFreqArray) free(coder.SymbolsFreqArray);
+		if(coder.decodeOutBufferArray) free(coder.decodeOutBufferArray);
+		if(coder.Sbox) free(coder.Sbox);
+		return 0;
+	}
 	// Initialize WJLDeCoder
-	InitializeWJLDeCoder(&coder, InByteArray, InByteArray_length);
+	InitializeWJLDeCoder(&coder, InByteArray, InByteArray_length, PasswordBuff, PasswordBuff_Length);
 	// Decrypt to 64 random numbers 64 = 512bit
-	if(decryptingRandomBytes(&coder,PasswordBuff, PasswordBuff_Length, InByteArray, InByteArray_length, 64)){
-		// Decrypt the HashValue and check
-		if(decryptingHashValue(&coder, InByteArray, InByteArray_length, PasswordBuff, PasswordBuff_Length, 65)){
-			// Decrypt the InByteArray_length
-			if(InArraylength = decryptingInArraylength(&coder, InByteArray, InByteArray_length)){
-				// Decrypt the datas
-				return WJLWeightedDecode(&coder, PasswordBuff, PasswordBuff_Length, InByteArray, InByteArray_length, InArraylength, OutByteArray_length);
+	if(decryptingRandomBytes(&coder, InByteArray, InByteArray_length, PasswordBuff, PasswordBuff_Length)){
+		// Decrypt the InByteArray_length
+		InArraylength = decryptingInArraylength(&coder, InByteArray, InByteArray_length, PasswordBuff, PasswordBuff_Length);
+		if(InArraylength !=0){
+			// Decrypt the datas
+			if(WJLWeightedDecode(&coder, InByteArray, InByteArray_length, PasswordBuff, PasswordBuff_Length, InArraylength, OutByteArray_length)){
+				OutBuff = (unsigned char *)malloc( InArraylength );
+				if(OutBuff == NULL){
+					if(coder.SymbolsFreqArray) free(coder.SymbolsFreqArray);
+					if(coder.decodeOutBufferArray) free(coder.decodeOutBufferArray);
+					if(coder.Sbox) free(coder.Sbox);
+					return 0;
+				}
+				memcpy(OutBuff, coder.decodeOutBufferArray, *OutByteArray_length);
+				if(coder.SymbolsFreqArray) free(coder.SymbolsFreqArray);
+				if(coder.decodeOutBufferArray) free(coder.decodeOutBufferArray);
+				if(coder.Sbox) free(coder.Sbox);
+				return OutBuff;
 			}else{
+				if(coder.SymbolsFreqArray) free(coder.SymbolsFreqArray);
+				if(coder.decodeOutBufferArray) free(coder.decodeOutBufferArray);
+				if(coder.Sbox) free(coder.Sbox);
 				return 0;
 			}
 		}else{
+			if(coder.SymbolsFreqArray) free(coder.SymbolsFreqArray);
+			if(coder.decodeOutBufferArray) free(coder.decodeOutBufferArray);
+			if(coder.Sbox) free(coder.Sbox);
 			return 0;
 		}
 	}else{
+		if(coder.SymbolsFreqArray) free(coder.SymbolsFreqArray);
+		if(coder.decodeOutBufferArray) free(coder.decodeOutBufferArray);
+		if(coder.Sbox) free(coder.Sbox);
 		return 0;
 	}
 }
